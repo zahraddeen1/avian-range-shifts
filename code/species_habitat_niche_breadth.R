53876c30835f92dc7d23455df8514f0d9f42a9da
gdicecco <gdicecco@live.unc.edu>
2021-02-09 13:57:28 -0600

    Update multipage PDFs, calculate dominance for species habitat niche

--- code/species_habitat_niche_breadth.R
+++ code/species_habitat_niche_breadth.R
@@ -4,22 +4,26 @@
 
 library(tidyverse)
 
+### Plotting theme
+
+theme_set(theme_classic())
+
 ### Source function to get eBirdst PIs for a species
 
-source("get_ebird_pis.R")
+source("code/get_ebird_pis.R")
 
 ### Species list
 
-species <- read_csv("species_list.csv")
+species <- read_csv("raw_data/species_list.csv")
 
-ebird_bbs <- ebirdst_runs %>% 
+ebird_bbs <- ebirdst::ebirdst_runs %>% 
   left_join(species, by = c("common_name" ="english_common_name")) %>%
   filter(!is.na(french_common_name))
 
 ### For each species, create a PI csv and save to /pis
 # easpho & easblu bug - skip
 
-p <- "/pis/"
+p <- "/bigdata/pis/"
 
 for(s in spp_left$species_code) {
   
@@ -31,16 +35,18 @@ for(s in spp_left$species_code) {
 
 ## Calculate Shannon Index of model land cover PI for each species
 
+these_predictors <- ebirdst::ebirdst_predictors
+
 lc_pred <- these_predictors %>%
   filter(!is.na(lc_class))
 
-spp_pi <- data.frame(spp_code = word(list.files("pis"), 1, 1, sep = "_"),
-                    filepath = list.files("pis"))
+spp_pi <- data.frame(spp_code = word(list.files("bigdata/pis"), 1, 1, sep = "_"),
+                    filepath = list.files("bigdata/pis"))
 
 spp_files <- spp_pi %>%
   group_by(spp_code, filepath) %>%
   nest() %>%
-  mutate(data = map(filepath, ~read_csv(paste0(getwd(),"/pis/",.))))
+  mutate(data = map(filepath, ~read_csv(paste0(getwd(),"/bigdata/pis/",.))))
 
 spp_hab_index <- spp_files %>%
   mutate(med_pi = map(data, ~{
@@ -74,49 +80,96 @@ spp_hab_index <- spp_files %>%
       
       h/hmax
       
-    }))
+    }),
+    lc_dominance = map_dbl(med_pi, ~{
+      df <- .
+      
+      lc_only <- df %>%
+        filter(Predictor %in% lc_pred$lc_class_label) %>%
+        na.omit()
+      
+      totalpi <- sum(lc_only$PI)
+      
+      maxpi <- max(lc_only$PI)
+      
+      maxpi/totalpi
+    }),
+    lc_dominance_class = map_chr(med_pi, ~{
+      df <- .
+      
+      lc_only <- df %>%
+        filter(Predictor %in% lc_pred$lc_class_label) %>%
+        na.omit()
+      
+      maxpi <- max(lc_only$PI)
+      
+      lc_only$Predictor[lc_only$PI == maxpi]
+    })) 
+
+spp_hab_breadth <- spp_hab_index %>%
+  select(-data, -med_pi) %>%
+  left_join(ebird_bbs, by = c("spp_code" = "species_code")) %>%
+  ungroup() %>%
+  select(spp_code, common_name, lc_evenness, lc_dominance, lc_dominance_class)
+write.csv(spp_hab_breadth, "derived_data/spp_habitat_niche_ebird.csv", row.names = F)
+
+# Plot histograms of Shannon E, dominance metrics
 
 ggplot(spp_hab_index, aes(x = lc_evenness)) + geom_histogram(col = "white") +
   labs(x = "Shannon Evenness - Habitat", y = "Species")
-ggsave("figures/shannonE_habitat_niche_hist.pdf")
+ggsave("figures/habitat_niche_shannonE_hist.pdf")
+
+ggplot(spp_hab_index, aes(x = lc_dominance)) + geom_histogram(col = "white") + 
+  labs(x = "Dominance - Habitat", y = "Species")
+ggsave("figures/habitat_niche_dominance_hist.pdf")
+
+ggplot(spp_hab_index, aes(x = lc_dominance_class)) + geom_histogram(stat = "count", col = "white") +
+  coord_flip() + labs(x = "Dominant class - Habitat", y = "Species")
+ggsave("figures/habitat_niche_dominance_class_counts.pdf")
+
+# Plot density of median PI values for each LC predictor class
+
+predictor_importance <- spp_hab_index %>%
+  select(-data) %>%
+  unnest(cols = c("med_pi")) %>%
+  filter(Predictor %in% lc_pred$lc_class_label)
+
+plot_lc_density <- function(data, Predictor) {
+  plot <- ggplot(data, aes(x = PI)) + 
+    geom_density(fill = "gray") + labs(x = "PI", title = Predictor)
+  return(plot)
+}
+
+plot_list <- predictor_importance %>%
+  group_by(Predictor) %>%
+  nest() %>%
+  pmap(plot_lc_density)
+
+multi_page <- gridExtra::marrangeGrob(plot_list, nrow = 2, ncol = 3)
+ggsave("figures/habitat_niche_landcover_density.pdf", multi_page, units = "in", height = 6, width = 10)
 
 ## Plot PIs and Shannon Index for each species in multi-page PDF
 
-plot_hab <- function(common_name, lc_evenness, data, ...) {
-  pi_long <- data %>%
-    pivot_longer(names_to = "Predictor", values_to = "PI", cols = everything()) %>%
-    filter(PI < quantile(PI, probs = 0.98, na.rm = T)) %>%
-    group_by(Predictor) %>%
-    mutate(med = median(PI))
+plot_hab <- function(common_name, lc_evenness, lc_dominance, med_pi, ...) {
+  pi_long <- med_pi %>%
+    filter(Predictor %in% lc_pred$lc_class_label) 
   
   theme_set(theme_classic(base_size = 15))
   
-  p <- ggplot(pi_long, aes(x = fct_reorder(Predictor, med), y = PI)) + geom_boxplot() +
-    labs(title = paste0(common_name, " E = ", round(lc_evenness, 3)),
+  ggplot(pi_long, aes(x = fct_reorder(Predictor, PI), y = PI)) + geom_point() +
+    labs(title = paste0(common_name),
          x = NULL, y = "Relative PI") +
+    annotate(geom = "text", x = 3, y = 0.5*max(pi_long$PI), label = paste0("E = ", round(lc_evenness, 2)), size = 6) +
+    annotate(geom = "text", x = 1, y = 0.5*max(pi_long$PI), label = paste0("D = ", round(lc_dominance, 2)), size = 6) +          
     coord_flip()
-  
-  print(p)
 }
 
-for(i in 1:8) {
-  start <- 45*i - 44
-  
-  if(i != 8) {
-    end <- 45*i }
-  else { end <- nrow(spp_hab_index) }
-  
-  pdf(paste0(getwd(), "/figures/spp_landcover_evenness_", start, "-", end, ".pdf"), height = 6, width = 8)
-  
-  subs <- spp_hab_index[start:end, ]
-  
-  subs %>%
-    left_join(ebird_bbs, by = c("spp_code" = "species_code")) %>%
-    arrange(aou) %>%
-    pmap(plot_hab)
-  
-  dev.off()
-}
+spp_plots <- spp_hab_index %>%
+  left_join(ebird_bbs, by = c("spp_code" = "species_code")) %>%
+  arrange(aou) %>%
+  pmap(plot_hab)
 
+spp_multi <- gridExtra::marrangeGrob(spp_plots, nrow = 1, ncol = 1)
+ggsave("figures/habitat_niche_spp_landcover_evenness.pdf", spp_multi, units = "in", height = 6, width = 8)
 
 
