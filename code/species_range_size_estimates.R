d90b170eefe472c6865ba67e4391c6812a46b6cb
Di Cecco <gdicecco@ad.unc.edu>
2021-06-14 10:16:56 -0400

    Range metrics with split range maps

--- code/species_range_size_estimates.R
+++ code/species_range_size_estimates.R
@@ -80,26 +80,43 @@ spp_overlap <- spp_list %>%
   mutate(range_overlap = purrr::map(file, ~{
     f <- .
     
+    print(f)
+    
     br <- read_sf(paste0(range_dir, f))
+    names(br)[1:14] <- toupper(names(br)[1:14])
     
     # Use extant (PRESENCE 1-3) breeding and resident ranges (SEASONAL 1-2) only
     # Re-project to land cover CRS
     breeding_range <- br %>%
       filter(PRESENCE %in% c(1:3), SEASONAL %in% c(1:2))
     
+    # Rasterize range
+    range_raster <- raster(crs = crs(br), vals = 0, resolution = c(1,1), ext = extent(c(-180, 180, -90, 90))) %>%
+      rasterize(br, .)
+    
+    range_cells <- sum(range_raster@data@values, na.rm = T)
+    
+    overlap_cells <- mask(range_raster, routes_raster)
+    
+    # Range polygon area
     range_size <- sum(st_area(br))
     
+    # Range polygon intersection with BBS
     overlap <- st_intersection(br, bbs_convex)
     
     overlap_area <- sum(st_area(overlap))
     
+    # Proportion of range polygon in BBS
     range_overlap <- overlap_area/range_size
     
-    data.frame(overlap = range_overlap)
+    # Number of cells in BBS
+    overlap_cells <- sum(overlap_cells@data@values[, 1], na.rm = T)
+    
+    data.frame(overlap = range_overlap, overlap_cells = overlap_cells)
     
   })) %>%
   unnest(cols = c("range_overlap"))
-# write.csv(dplyr::select(spp_overlap, -climate_vol), "derived_data/spp_bbs_range_overlap.csv", row.names = F)
+write.csv(dplyr::select(spp_overlap, -climate_vol), "derived_data/spp_bbs_range_overlap.csv", row.names = F)
 
 spp_overlap <- read_csv("derived_data/spp_bbs_range_overlap.csv")
 
@@ -111,13 +128,25 @@ spp <- spp_overlap %>%
 ## Fit concave hulls function, needs AOU and BBS counts data for that species
 
 concave_area <- function(aou, df) {
+  print(aou)
+  
+  f <- spp_list$file[spp_list$aou == aou]
+  
+  br <- read_sf(paste0(range_dir, f))
+  names(br)[1:14] <- toupper(names(br)[1:14])
+  
+  # Use extant (PRESENCE 1-3) breeding and resident ranges (SEASONAL 1-2) only
+  # Re-project to land cover CRS
+  breeding_range <- br %>%
+    filter(PRESENCE %in% c(1:3), SEASONAL %in% c(1:2))
+  
   concave <- routes_subs %>%
     filter(cell_id %in% df$cell_id) %>%
     concaveman(., concavity = 2)
   
-  concave_sub <- st_intersection(concave, br)
+  concave_sub <- st_intersection(concave, breeding_range)
   
-  concave_nobr <- st_difference(concave, br)
+  concave_nobr <- st_difference(concave, breeding_range)
   
   concave_nobr_cast <- st_cast(concave_nobr, "POLYGON") %>%
     mutate(polygonID = row.names(.))
@@ -155,7 +184,7 @@ concave_area <- function(aou, df) {
              min_lat = min(df$lat_cell),
              min_lon = min(df$lon_cell),
              total_cells = n_distinct(df$cell_id),
-             area = sum(st_area(concave))))
+             area = sum(st_area(concave_all))))
 }
 
 possibly_concave_area <- possibly(concave_area, NA)
@@ -229,7 +258,7 @@ range_samp <- data.frame(sim = c(1:499)) %>%
       left_join(spp_counts_t2, by = c("aou"), suffix = c("_t1", "_t2")) %>%
       left_join(spp_overlap, by = c("aou")) %>%
       mutate(delta_area = (area_t2 - area_t1)/area_t1,
-             delta_occ = (total_cells_t2 - total_cells_t1)/overlap)
+             delta_occ = (total_cells_t2 - total_cells_t1)/overlap_cells)
     
     spp_change
   }))
@@ -237,7 +266,9 @@ range_samp <- data.frame(sim = c(1:499)) %>%
 range_metrics <- range_samp %>%
   dplyr::select(-data) %>%
   unnest(cols = c("range_metrics"))
-write.csv(range_metrics, "derived_data/range_metrics_sampled.csv", row.names = F)
+# write.csv(range_metrics, "derived_data/range_metrics_sampled.csv", row.names = F)
+
+range_metrics <- read_csv("derived_data/range_metrics_sampled.csv")
 
 range_metrics_sum <- range_metrics %>%
   mutate(delta_lat = (max_lat_t2 - min_lat_t2) - (max_lat_t1 - min_lat_t1),
@@ -250,7 +281,8 @@ range_metrics_sum <- range_metrics %>%
             mean_delta_lat = mean(delta_lat, na.rm = T),
             sd_delta_lat = sd(delta_lat, na.rm = T),
             mean_delta_lon = mean(delta_lon, na.rm = T),
-            sd_delta_lon = sd(delta_lon, na.rm = T))
+            sd_delta_lon = sd(delta_lon, na.rm = T)) %>%
+  filter(!is.na(mean_delta_area))
 
 area_occ_r <- cor(range_metrics_sum$mean_delta_area, range_metrics_sum$mean_delta_occ, use = "pairwise.complete.obs")
 
@@ -258,7 +290,7 @@ area_plot <- ggplot(range_metrics_sum, aes(x = mean_delta_area)) + geom_histogra
 occ_plot <- ggplot(range_metrics_sum, aes(x = mean_delta_occ)) + geom_histogram(col = "white") + labs(x = "Change in occupancy")
 area_occ <- ggplot(range_metrics_sum, aes(x = mean_delta_area, y = mean_delta_occ)) + geom_point(alpha = 0.5) + 
   labs(x = "Change in area (km^2)", y = "Change in occupancy") + geom_hline(yintercept = 0) + geom_vline(xintercept = 0) +
-  annotate(x = 200, y = -1, geom = "text", label = paste0("r = ", round(area_occ_r, 2)))
+  annotate(x = 300, y = -0.3, geom = "text", label = paste0("r = ", round(area_occ_r, 2)))
 cowplot::plot_grid(area_plot, occ_plot, area_occ, nrow = 2)
 ggsave('figures/range_area_occ_hist.pdf', units = "in", height = 8, width = 10)
 
@@ -268,7 +300,7 @@ ggsave('figures/range_area_occ_hist.pdf', units = "in", height = 8, width = 10)
 ## Mean, conf int of range area and range occ deltas
 
 pdf("figures/spp_range_estimates.pdf", height = 8, width = 10)
-for(s in spp$aou) {
+for(s in range_metrics_sum$aou) {
   name <- spp$english_common_name[spp$aou == s]
   
   f <- spp$file[spp$aou == s]
@@ -277,6 +309,7 @@ for(s in spp$aou) {
   d_occ <- range_metrics_sum$mean_delta_occ[range_metrics_sum$aou == s]
   
   br <- read_sf(paste0(range_dir, f))
+  names(br)[1:14] <- toupper(names(br)[1:14])
   
   # Use extant (PRESENCE 1-3) breeding and resident ranges (SEASONAL 1-2) only
   # Re-project to land cover CRS
@@ -308,32 +341,97 @@ for(s in spp$aou) {
     dplyr::select(-cell_id, -n_routes)
 
   if(nrow(spp_routes_t1) >= 4) {
+    # Raster of occurrences time 1
   pts_t1 <- as(spp_routes_t1, "Spatial")
   
   routes_raster_t1 <- raster(crs = crs(pts_t1), vals = 0, resolution = c(1,1), ext = extent(c(-180, 180, -90, 90))) %>%
     rasterize(pts_t1, .)
 
+  # Raster of occurrences time 2
   pts_t2 <- as(spp_routes_t2, "Spatial")
   
   routes_raster_t2 <- raster(crs = crs(pts_t2), vals = 0, resolution = c(1,1), ext = extent(c(-180, 180, -90, 90))) %>%
     rasterize(pts_t2, .)
   
-    # Spp concave hull
+    # Spp concave hull time 1
+  
+  concave <- spp_routes_t1 %>%
+    concaveman(., concavity = 2) %>%
+    st_buffer(0)
     
-    concave <- concaveman(spp_routes_t1, concavity = 2)
-    concave2 <- concaveman(spp_routes_t2, concavity = 2)
+  concave_sub <- st_intersection(concave, st_buffer(breeding_range, 0))
+  
+  concave_nobr <- st_difference(concave, st_buffer(breeding_range, 0))
+  
+  concave_nobr_cast <- st_cast(concave_nobr, "POLYGON") %>%
+    mutate(polygonID = row.names(.))
+  
+  # does concave area have at least 2 BBS routes in it?
+  routes_sf <- RT1.routes %>%
+    filter(stateroute %in% spp_counts_t1$stateroute) %>%
+    st_as_sf(coords = c("longitude", "latitude")) %>%
+    st_set_crs(4326) %>%
+    st_intersection(concave_nobr_cast) %>%
+    group_by(polygonID) %>%
+    mutate(n_routes = n_distinct(stateroute)) %>%
+    filter(n_routes >= 2)
+  
+  if(nrow(routes_sf) > 0) {
+    add_shp <- concave_nobr_cast %>%
+      filter(polygonID %in% routes_sf$polygonID)
+    
+    concave_all <- st_union(add_shp, st_buffer(concave_sub, 0))
+    
+    
+  } else {
+    concave_all <- concave_sub
+  }
+  
+  # Spp concave hull time 2
+  
+  concave2 <- spp_routes_t2 %>%
+    concaveman(., concavity = 2) %>%
+    st_buffer(0)
+  
+  concave_sub2 <- st_intersection(concave2, st_buffer(breeding_range, 0))
+  
+  concave_nobr2 <- st_difference(concave2, st_buffer(breeding_range, 0))
+  
+  concave_nobr_cast2 <- st_cast(concave_nobr2, "POLYGON") %>%
+    mutate(polygonID = row.names(.))
+  
+  # does concave area have at least 2 BBS routes in it?
+  routes_sf2 <- RT1.routes %>%
+    filter(stateroute %in% spp_counts_t2$stateroute) %>%
+    st_as_sf(coords = c("longitude", "latitude")) %>%
+    st_set_crs(4326) %>%
+    st_intersection(concave_nobr_cast2) %>%
+    group_by(polygonID) %>%
+    mutate(n_routes = n_distinct(stateroute)) %>%
+    filter(n_routes >= 2)
+  
+  if(nrow(routes_sf2) > 0) {
+    add_shp2 <- concave_nobr_cast2 %>%
+      filter(polygonID %in% routes_sf2$polygonID)
+    
+    concave_all2 <- st_union(add_shp2, st_buffer(concave_sub2, 0))
+    
+    
+  } else {
+    concave_all2 <- concave_sub2
+  }
     
     range_pol_1 <- tm_shape(na_map) + tm_polygons() + tm_shape(breeding_range) + tm_polygons(col = "skyblue") + 
       tm_shape(routes_raster_t1) + tm_raster(col = "n_bins", legend.show = F, palette = "Reds") + tm_layout(title = name)
     
     range_hulls_1 <- tm_shape(na_map) + tm_polygons() + 
-      tm_shape(concave) + tm_polygons(col = "skyblue4", alpha = 0.5)
+      tm_shape(concave_all) + tm_polygons(col = "skyblue4", alpha = 0.5)
     
     range_pol_2 <- tm_shape(na_map) + tm_polygons() + tm_shape(breeding_range) + tm_polygons(col = "skyblue") + 
-      tm_shape(routes_raster_t2) + tm_raster(col = "n_bins", legend.show = F, palette = "Reds") + tm_layout(title = paste0("change area = ", round(d_area), "\nchange occ = ", round(d_occ, 2)))
+      tm_shape(routes_raster_t2) + tm_raster(col = "n_bins", legend.show = F, palette = "Reds") + tm_layout(title = paste0("change area = ", round(d_area, 2), "\nchange occ = ", round(d_occ, 2)))
     
     range_hulls_2 <- tm_shape(na_map) + tm_polygons() + 
-      tm_shape(concave2) + tm_polygons(col = "skyblue4", alpha = 0.5)
+      tm_shape(concave_all2) + tm_polygons(col = "skyblue4", alpha = 0.5)
     
     range_maps <- tmap_arrange(range_pol_1, range_hulls_1, range_pol_2, range_hulls_2, nrow =2)
     
