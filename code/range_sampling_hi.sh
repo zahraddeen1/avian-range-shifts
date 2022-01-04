17e5fb2dfa1c1284f6a2c302e4618d5e87ca5991
gdicecco <gdicecco@live.unc.edu>
2022-01-04 12:38:12 -0600

    Add lo/hi sensitivity analyses for concavity

---
+++ code/range_sampling_hi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=spp_range_hi
+#SBATCH --time=5-0
+#SBATCH -N 1
+#SBATCH -n 10
+#SBATCH --mem-per-cpu=10G
+
+# Load modules
+module load r/4.0.1
+module load geos/3.8.1
+module load gdal/3.2.1
+module load proj/7.2.1
+
+# Run rscript
+Rscript species_range_estimates_longleaf_hi.R
