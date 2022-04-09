79dddba34743a168e90d028a8da685f199410538
gdicecco <gdicecco@live.unc.edu>
2022-04-09 13:16:54 -0500

    weight responses in SEM by confint

--- code/sem_fit.R
+++ code/sem_fit.R
@@ -14,28 +14,35 @@ diet <- read_csv("derived_data/diet_niche_breadth.csv")
 range <- read_csv("derived_data/range_metrics_sampled.csv") %>%
   group_by(aou) %>%
   summarize(mean_area = mean((area_t2-area_t1)/area_t1, na.rm = T),
-            mean_occ = mean((total_cells_t2 - total_cells_t1)/overlap_cells, na.rm = T))
+            mean_occ = mean((total_cells_t2 - total_cells_t1)/overlap_cells, na.rm = T),
+            sd_area = sd((area_t2-area_t1)/area_t1, na.rm = T),
+            sd_occ = sd((total_cells_t2 - total_cells_t1)/overlap_cells, na.rm = T))
 
 range_lo_spp <- read_csv("derived_data/range_metrics_sampled_lo_concav_fix_spp.csv") %>%
   group_by(aou) %>%
   summarize(mean_area = mean((area_t2-area_t1)/area_t1, na.rm = T),
-            mean_occ = mean((total_cells_t2 - total_cells_t1)/overlap_cells, na.rm = T))
+            mean_occ = mean((total_cells_t2 - total_cells_t1)/overlap_cells, na.rm = T),
+            sd_area = sd((area_t2-area_t1)/area_t1, na.rm = T),
+            sd_occ = sd((total_cells_t2 - total_cells_t1)/overlap_cells, na.rm = T))
 
 range_lo <- read_csv("derived_data/range_metrics_sampled_lo_concav.csv") %>%
   group_by(aou) %>%
   summarize(mean_area = mean((area_t2-area_t1)/area_t1, na.rm = T),
-            mean_occ = mean((total_cells_t2 - total_cells_t1)/overlap_cells, na.rm = T)) %>%
-  bind_rows(range_lo_spp)
+            mean_occ = mean((total_cells_t2 - total_cells_t1)/overlap_cells, na.rm = T),
+            sd_area = sd((area_t2-area_t1)/area_t1, na.rm = T),
+            sd_occ = sd((total_cells_t2 - total_cells_t1)/overlap_cells, na.rm = T))
 
 range_hi <- read_csv("derived_data/range_metrics_sampled_hi_concav.csv") %>%
   group_by(aou) %>%
   summarize(mean_area = mean((area_t2-area_t1)/area_t1, na.rm = T),
-            mean_occ = mean((total_cells_t2 - total_cells_t1)/overlap_cells, na.rm = T))
-
+            mean_occ = mean((total_cells_t2 - total_cells_t1)/overlap_cells, na.rm = T),
+            sd_area = sd((area_t2-area_t1)/area_t1, na.rm = T),
+            sd_occ = sd((total_cells_t2 - total_cells_t1)/overlap_cells, na.rm = T))
 
 poptrend <- read_csv("raw_data/BBS_1966-2017_core_trend_revised_v2.csv", 
                      col_types = cols(AOU = col_double())) %>%
-  filter(Region == "SU1")
+  filter(Region == "SU1") %>%
+  mutate(ci95 = `97.5%CI` - `2.5%CI`)
 
 ## Write range areas csv
 area_cc_var <- data.frame(concav = 2, delta_area = range$mean_area, aou = range$aou) %>%
@@ -44,30 +51,43 @@ area_cc_var <- data.frame(concav = 2, delta_area = range$mean_area, aou = range$
 write.csv(area_cc_var, "derived_data/delta_area_concav_sensitivity.csv", row.names = F)
 
 ## Model table
+## Weight range area, occupancy, and poptrend calcs by std deviation
 
 range_mod <- clim %>%
   left_join(hab, by = c("species_code" = "spp")) %>%
   left_join(dplyr::select(diet, aou, shannonE_diet), by = c("aou")) %>%
   left_join(range) %>%
-  left_join(dplyr::select(poptrend, AOU, Trend), by = c("aou" = "AOU")) %>%
+  left_join(dplyr::select(poptrend, AOU, Trend, ci95), by = c("aou" = "AOU")) %>%
   mutate_at(c("ssi"), ~.*-1) %>%
-  filter(!is.na(mean_area) & !is.na(ssi))
+  filter(!is.na(mean_area) & !is.na(ssi)) %>%
+  mutate_at(c("sd_area", "sd_occ"), ~ifelse(. == 0, .+0.05*min(.[. != 0]), .)) %>%
+  mutate(area_wt = sd_area*1.96,
+         occ_wt = sd_occ*1.96,
+         trend_wt = ci95)
 
 range_mod_lo <- clim %>%
   left_join(hab, by = c("species_code" = "spp")) %>%
   left_join(dplyr::select(diet, aou, shannonE_diet), by = c("aou")) %>%
   left_join(range_lo) %>%
-  left_join(dplyr::select(poptrend, AOU, Trend), by = c("aou" = "AOU")) %>%
+  left_join(dplyr::select(poptrend, AOU, Trend, ci95), by = c("aou" = "AOU")) %>%
   mutate_at(c("ssi"), ~.*-1) %>%
-  filter(!is.na(mean_area) & !is.na(ssi))
+  filter(!is.na(mean_area) & !is.na(ssi)) %>%
+  mutate_at(c("sd_area", "sd_occ"), ~ifelse(. == 0, .+0.05*min(.[. != 0]), .)) %>%
+  mutate(area_wt = sd_area*1.96,
+         occ_wt = sd_occ*1.96,
+         trend_wt = ci95)
 
 range_mod_hi <-  clim %>%
   left_join(hab, by = c("species_code" = "spp")) %>%
   left_join(dplyr::select(diet, aou, shannonE_diet), by = c("aou")) %>%
   left_join(range_hi) %>%
-  left_join(dplyr::select(poptrend, AOU, Trend), by = c("aou" = "AOU")) %>%
+  left_join(dplyr::select(poptrend, AOU, Trend, ci95), by = c("aou" = "AOU")) %>%
   mutate_at(c("ssi"), ~.*-1) %>%
-  filter(!is.na(mean_area) & !is.na(ssi))
+  filter(!is.na(mean_area) & !is.na(ssi)) %>%
+  mutate_at(c("sd_area", "sd_occ"), ~ifelse(. == 0, .+0.05*min(.[. != 0]), .)) %>%
+  mutate(area_wt = sd_area*1.96,
+         occ_wt = sd_occ*1.96,
+         trend_wt = ci95)
 
 ## Phylo trees
 
@@ -101,14 +121,17 @@ spp_psem <- psem(
   gls(mean_occ ~ shannonE_diet + climate_vol + ssi + Trend, 
       na.action = na.omit,
       correlation = corBrownian(0.5, tree1),
+      weights = ~1/occ_wt,
       data = spp_data),
   gls(Trend ~ ssi + climate_vol + shannonE_diet,
       correlation = corBrownian(0.5, tree1),
       na.action = na.omit,
+      weights = ~1/trend_wt,
       data = spp_data),
   gls(mean_area ~ ssi + climate_vol + shannonE_diet + Trend,
       na.action = na.omit,
       correlation = corBrownian(0.5, tree1),
+      weights= ~1/area_wt,
       data = spp_data),
   data = spp_data)
 
@@ -120,7 +143,7 @@ plot(spp_psem,
        shape = "rectangle", color = "black", x = 1:6, y = 1:3,
        width = 1))
 
-spp_psem_simple <- spp_psem <- psem(
+spp_psem_simple <- psem(
   gls(mean_occ ~ shannonE_diet + climate_vol + ssi + Trend, 
       na.action = na.omit,
       data = spp_data),
@@ -156,14 +179,17 @@ spp_psem_lo <- psem(
   gls(mean_occ ~ shannonE_diet + climate_vol + ssi + Trend, 
       na.action = na.omit,
       correlation = corBrownian(0.5, tree1),
+      weights = ~1/occ_wt,
       data = spp_data_lo),
   gls(Trend ~ ssi + climate_vol + shannonE_diet,
       correlation = corBrownian(0.5, tree1),
       na.action = na.omit,
+      weights = ~1/trend_wt,
       data = spp_data_lo),
   gls(mean_area ~ ssi + climate_vol + shannonE_diet + Trend,
       na.action = na.omit,
       correlation = corBrownian(0.5, tree1),
+      weights= ~1/area_wt,
       data = spp_data_lo),
   data = spp_data_lo)
 summary(spp_psem_lo)
@@ -192,16 +218,18 @@ spp_psem_hi <- psem(
   gls(mean_occ ~ shannonE_diet + climate_vol + ssi + Trend, 
       na.action = na.omit,
       correlation = corBrownian(0.5, tree1),
+      weights = ~1/occ_wt,
       data = spp_data_hi),
   gls(Trend ~ ssi + climate_vol + shannonE_diet,
       correlation = corBrownian(0.5, tree1),
       na.action = na.omit,
+      weights = ~1/trend_wt,
       data = spp_data_hi),
   gls(mean_area ~ ssi + climate_vol + shannonE_diet + Trend,
       na.action = na.omit,
       correlation = corBrownian(0.5, tree1),
+      weights= ~1/area_wt,
       data = spp_data_hi),
-  data = spp_data_hi)
+  data = spp_data)
 summary(spp_psem_hi)
-
 
